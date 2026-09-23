007b60d916850591bbe61e3c46b5ced0334c1ce4
Minty <mmeminty@gmail.com>
2026-09-23 13:59:23 +0200

    Feed MozJpeg raw pixels instead of PNG round-trip

--- Code/ImageUtils/MozJpeg.cs
+++ Code/ImageUtils/MozJpeg.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using MozJpegSharp;
 using ImageMagick;
@@ -9,44 +8,30 @@ namespace Cupscale.ImageUtils
 {
     class MozJpeg
     {
-        public static Bitmap ReadImage(string path)
-        {
-            return (Bitmap)ImgUtils.GetImage(path);
-        }
-
-        public static Bitmap ReadImage(MagickImage image)
-        {
-            using (MemoryStream memStream = new MemoryStream())
-            {
-                image.Write(memStream);
-
-                return new Bitmap(memStream);
-            }
-        }
-
         public static void Encode(MagickImage image, string outPath, int q, bool chromaSubSample = true)
         {
-            Bitmap bitmap_image = ReadImage(image);
-            Encode_Bitmap(bitmap_image, outPath, q, chromaSubSample);
+            int width = (int)image.Width;
+            int height = (int)image.Height;
+            byte[] bgr;
+
+            using (var pixels = image.GetPixelsUnsafe())    // Raw 8-bit BGR, no intermediate PNG/Bitmap
+                bgr = pixels.ToByteArray(PixelMapping.BGR);
+
+            Encode(bgr, width, height, outPath, q, chromaSubSample);
         }
 
-        public static void Encode(string path, string outpath, int q, bool chromaSubSample = true)
-        {
-            Bitmap bitmap_image = ReadImage(path);
-            Encode_Bitmap(bitmap_image, outpath, q, chromaSubSample);
-        }
-
-        public static void Encode_Bitmap(Bitmap bmp, string outPath, int q, bool chromaSubSample = true)
+        public static void Encode(byte[] bgr, int width, int height, string outPath, int q, bool chromaSubSample = true)
         {
             try
             {
-                var commpressor = new TJCompressor();
-                byte[] compressed;
-                TJSubsamplingOption subSample = TJSubsamplingOption.Chrominance420;
-                if (!chromaSubSample)
-                    subSample = TJSubsamplingOption.Chrominance444;
-                compressed = commpressor.Compress(bmp, subSample, q, TJFlags.None);
-                File.WriteAllBytes(outPath, compressed);
+                TJSubsamplingOption subSample = chromaSubSample ? TJSubsamplingOption.Chrominance420 : TJSubsamplingOption.Chrominance444;
+
+                using (var compressor = new TJCompressor())
+                {
+                    byte[] compressed = compressor.Compress(bgr, width * 3, width, height, PixelFormat.Format24bppRgb, subSample, q, TJFlags.None);   // GDI+ 24bppRgb = BGR byte order
+                    File.WriteAllBytes(outPath, compressed);
+                }
+
                 Logger.Log("[MozJpeg] Written image to " + outPath);
             }
             catch (TypeInitializationException e)
